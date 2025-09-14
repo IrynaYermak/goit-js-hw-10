c0783da5c469284f63be4e907b6c30c1f8027024
Iryna Yermak <1991irenka@gmail.com>
2025-09-14 09:52:37 +0100

    made snackbar, styles corrections

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -22,11 +22,33 @@ function promiseCreator(event) {
 
   prom
     .then(text => {
-      return alert(text);
+      return iziToast.success({
+        message: text,
+        timeout: 3000,
+        messageColor: 'white',
+        position: 'topRight',
+        backgroundColor: '#59a10d',
+        borderBottom: '2px solid #b5ea7c',
+        progressBar: false,
+        close: false,
+        icon: '',
+        width: '383px',
+        // iconColor: 'white',
+        // iconUrl: '/src/img/symbol-defs.svg#icon-check-circle',
+      });
     })
     .catch(errorText => {
-      return alert(errorText);
+      return iziToast.error({
+        message: errorText,
+        timeout: 3000,
+        messageColor: 'white',
+        position: 'topRight',
+        backgroundColor: '#ef4040',
+        progressBar: false,
+        close: false,
+        icon: '',
+      });
     });
 
-  event.target.elements.delay.value = '';
+  event.target.reset();
 }
